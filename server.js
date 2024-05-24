5412b799b771e1b7e4831ab35bf4053319b7c2d5
hubhacker <shreyathirumala@gmail.com>
2024-05-24 13:00:31 -0400

    Changed login

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
-const db = require('./database');
 
 const app = express();
 const port = 3000;
@@ -15,54 +13,33 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-// Helper functions
-const findUserByUsername = (username, callback) => {
-    db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
-        callback(err, row);
-    });
+// Sample user data 
+const users = {
+    user1: 'password1',
+    user2: 'password2'
 };
 
-const createUser = (username, password, callback) => {
-    bcrypt.hash(password, 10, (err, hash) => {
-        if (err) return callback(err);
-        db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, hash], function (err) {
-            callback(err, this.lastID);
-        });
-    });
-};
-
-// Routes
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    findUserByUsername(username, (err, user) => {
-        if (err) return res.json({ success: false, message: 'Error during login' });
-        if (!user) return res.json({ success: false, message: 'Invalid username or password' });
-
-        bcrypt.compare(password, user.password, (err, result) => {
-            if (result) {
-                req.session.user = user.username;
-                res.json({ success: true });
-            } else {
-                res.json({ success: false, message: 'Invalid username or password' });
-            }
-        });
-    });
+    if (users[username] && users[username] === password) {
+        req.session.user = username;
+        res.json({ success: true });
+    } else {
+        res.json({ success: false, message: 'Invalid username or password' });
+    }
 });
 
 app.post('/signup', (req, res) => {
     const { username, password } = req.body;
 
-    findUserByUsername(username, (err, user) => {
-        if (err) return res.json({ success: false, message: 'Error during signup' });
-        if (user) return res.json({ success: false, message: 'Username already taken' });
-
-        createUser(username, password, (err) => {
-            if (err) return res.json({ success: false, message: 'Error creating user' });
-            req.session.user = username;
-            res.json({ success: true });
-        });
-    });
+    if (users[username]) {
+        res.json({ success: false, message: 'Username already taken' });
+    } else {
+        users[username] = password;
+        req.session.user = username;
+        res.json({ success: true });
+    }
 });
 
 app.get('/', (req, res) => {
